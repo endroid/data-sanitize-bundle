1a2e7e8e5fd53e9465a4c35f56feb44a1cbf0422
Jeroen van den Enden <info@endroid.nl>
2020-05-11 20:32:39 +0000

    Rename variables

--- src/Controller/MergeController.php
+++ src/Controller/MergeController.php
@@ -30,16 +30,16 @@ final class MergeController
 
     public function __invoke(Request $request, string $name): Response
     {
-        $sources = $request->query->get('sources');
-        if (0 == count($sources)) {
+        $sourceIds = $request->query->get('sources');
+        if (0 == count($sourceIds)) {
             return new JsonResponse([
                 'success' => false,
                 'error' => 'Invalid sources',
             ]);
         }
 
-        $target = $request->query->get('target');
-        if (is_null($target)) {
+        $targetId = $request->query->get('target');
+        if (is_null($targetId)) {
             return new JsonResponse([
                 'success' => false,
                 'error' => 'Invalid target',
@@ -47,7 +47,7 @@ final class MergeController
         }
 
         $sanitizer = $this->sanitizerFactory->create($this->configuration->getClass($name));
-        $sanitizer->merge($sources, $target);
+        $sanitizer->merge($sourceIds, $targetId);
 
         return new JsonResponse([
             'success' => true,
